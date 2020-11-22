0b602662522b0fd8994a79ee641a66d354e0af59
Tony Wong <manifaust@gmail.com>
2020-11-22 18:35:41 -0500

    Add both register.go, doc.go, and changed types.go to match openshift-evangelists/crd-code-generation

--- kcrl-client-go/pkg/apis/mygroup/v1alpha1/types.go
+++ kcrl-client-go/pkg/apis/mygroup/v1alpha1/types.go
@@ -5,49 +5,30 @@ import (
 )
 
 // +genclient
+// +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-const (
-	PhasePending = "PENDING"
-	PhaseRunning = "RUNNING"
-	PhaseDone    = "DONE"
-)
-
-// AtSpec defines the desired state of At
-type AtSpec struct {
-	// Schedule is the desired time the command is supposed to be executed.
-	// Note: the format used here is UTC time https://www.utctime.net
-	Schedule string `json:"schedule,omitempty"`
-	// Command is the desired command (executed in a Bash shell) to be executed.
-	Command string `json:"command,omitempty"`
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-// AtStatus defines the observed state of At
-type AtStatus struct {
-	// Phase represents the state of the schedule: until the command is executed
-	// it is PENDING, afterwards it is DONE.
-	Phase string `json:"phase,omitempty"`
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// At runs a command at a given schedule.
-type At struct {
+// Database describes a database.
+type Database struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   AtSpec   `json:"spec,omitempty"`
-	Status AtStatus `json:"status,omitempty"`
+	Spec DatabaseSpec `json:"spec"`
+}
+
+// DatabaseSpec is the spec for a Foo resource
+type DatabaseSpec struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Encoding string `json:"encoding,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// AtList contains a list of At
-type AtList struct {
+// DatabaseList is a list of Database resources
+type DatabaseList struct {
 	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []At `json:"items"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Database `json:"items"`
 }
