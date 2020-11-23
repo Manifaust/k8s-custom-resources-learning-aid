b99abb653a59367fc86ef303e07dfce88d1fee7a
Tony Wong <manifaust@gmail.com>
2020-11-22 19:37:59 -0500

    add script to call generate, switch to use examples from sample-controller

--- kcrl-client-go/go.mod
+++ kcrl-client-go/go.mod
@@ -3,6 +3,6 @@ module github.com/manifaust/k8s-custom-resource-lessons/kcrl-client-go
 go 1.15
 
 require (
-	github.com/openshift-evangelists/crd-code-generation v0.0.0-20180227162415-5ae21b8b0582
 	k8s.io/apimachinery v0.19.4
+	k8s.io/code-generator v0.19.4
 )
--- kcrl-client-go/pkg/apis/samplecontroller/v1alpha1/doc.go
+++ kcrl-client-go/pkg/apis/samplecontroller/v1alpha1/doc.go
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package mygroup
+// +k8s:deepcopy-gen=package
+// +groupName=samplecontroller.k8s.io
 
-const (
-	GroupName = "mygroup.com"
-)
+// Package v1alpha1 is the v1alpha1 version of the API.
+package v1alpha1 // import "k8s.io/sample-controller/pkg/apis/samplecontroller/v1alpha1"
